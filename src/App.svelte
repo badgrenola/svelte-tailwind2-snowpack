2101d3be21057e269a7f3b4c2d9d36b3d2c0676c
Matt Brealey <matt@mattbrealey.com>
2022-06-23 13:29:41 +0100

    Add basic contact details

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
   import "./index.css"
   import Header from './Components/Header.svelte'
-  import Contact from './Components/Contact.svelte'
   import Experience from './Components/Experience.svelte'
 
   import { details } from '/companies/apple'
@@ -11,8 +10,6 @@
 <main class="w-full font-inter p-8 leading-6 text-sm">
   <div class="container mx-auto lg:max-w-4xl print:mx-0 print:max-w-none">
     <Header headerParas={details.headerParas} />
-    <hr class="mt-8 sm:mt-12"/>
-    <Contact />
     <hr class="mt-8 sm:mt-12"/>
     <Experience jobs={details.jobs} hideSkills={details.hideSkills}/>
   </div>
--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -16,5 +16,19 @@
       <p>{@html para}</p>
     {/each}
   </div>  
+  <div class="flex">
+    <div class="w-20 flex flex-col font-bold space-y-2">
+      <span>Location</span>
+      <span>Web</span>
+      <span>Email</span>
+      <span>Phone</span>
+    </div>
+    <div class="flex flex-col space-y-2">
+      <span>Bristol, UK</span>
+      <span>mattbrealey.com</span>
+      <span>matt@mattbrealey.com</span>
+      <span>+44 7846 639 461</span>
+    </div>
+  </div>
 </section>
 <!-- <p>Whilst a large percentage of my freelance work currently involves both the Design and VFX industries, over the past several years I have also been using my skillset to develop novel applications to both process and increase the accessibility of publically available raw space imagery. Once such example of this is AreoBrowser, a web-app for viewing thousands of 3D models of the Martian surface.</p> -->
