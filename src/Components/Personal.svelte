ca41af184aac1aa9095c432f16d819311b63154c
Matt Brealey <matt@mattbrealey.com>
2022-06-23 15:51:55 +0100

    Add AB desc

--- src/Components/Personal.svelte
+++ src/Components/Personal.svelte
@@ -13,7 +13,10 @@
   </div>
 
   <div class="mt-2 sm:mt-4 flex flex-col space-y-4 text-justify print:mt-2">
-    <p>Some words...</p>
+    <p><i>AreoBrowser</i> is a web-app I've created that allows users to easily explore all of the latest Martian terrain data and rover imagery, directly in their browser, on desktop, tablet and mobile, without any previous knowledge of the cross-agency missions required.</p>
+    <p>The 3D terrain aspect of the app involves offline data processing (with a pipeline incorporating entirely custom Swift, Node, and python applications to parse available raw imagery) alongside custom GLSL shaders which allow for dynamic geometry creation directly in the browser, at a low overhead where needed whilst maintaining a high-level of data accuracy.</p>
+    <p>The rover imagery aspect involves dynamically grouping available imagery into easily contextualised image views - images involved in panoramas are automatically stitched together; stereo images are presented in numerous useful ways (anaglyph, side-by-side, etc.); sequences of images appear with playback controls; depth data is displayed as navigable 3D models; all with automatic quality switching based on the current device.</p>
+    <p>I am incredibly proud of this project. To learn more, visit : <b>AreoBrowser.com</b></p>
   </div>
 
 </section>
